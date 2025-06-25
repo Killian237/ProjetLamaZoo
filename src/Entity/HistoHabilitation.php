dc9be1dc50d5f0b0009bb5fa2536219b417c6336
pierre3150 <pierre.parain315@gmail.com>
2025-06-25 14:54:06 +0200

    Ajout de la bdd

---
+++ src/Entity/HistoHabilitation.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HistoHabilitationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HistoHabilitationRepository::class)]
+class HistoHabilitation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateHeure = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $action = null;
+
+    #[ORM\ManyToOne(inversedBy: 'histoHabilitations')]
+    private ?Application $application = null;
+
+    #[ORM\ManyToOne(inversedBy: 'histoHabilitations')]
+    private ?Personnel $personnel = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateHeure(): ?\DateTime
+    {
+        return $this->dateHeure;
+    }
+
+    public function setDateHeure(\DateTime $dateHeure): static
+    {
+        $this->dateHeure = $dateHeure;
+
+        return $this;
+    }
+
+    public function getAction(): ?string
+    {
+        return $this->action;
+    }
+
+    public function setAction(string $action): static
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    public function getApplication(): ?Application
+    {
+        return $this->application;
+    }
+
+    public function setApplication(?Application $application): static
+    {
+        $this->application = $application;
+
+        return $this;
+    }
+
+    public function getPersonnel(): ?Personnel
+    {
+        return $this->personnel;
+    }
+
+    public function setPersonnel(?Personnel $personnel): static
+    {
+        $this->personnel = $personnel;
+
+        return $this;
+    }
+}
