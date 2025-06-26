307e5a3536b987056128e73b40d7bd71bb400003
Killian237 <killian.henriet@ecoles-epsi.net>
2025-06-26 18:02:29 +0200

    ajout securite blocage temporaire

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,27 +6,41 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Service\LoginAttemptService;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\PersonnelRepository;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+    public function login(Request $request, PersonnelRepository $personnelRepository): Response
+{
+    $remainingTime = null;
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    // Récupère le dernier email tenté
+    $email = $request->getSession()->get('_security.last_username');
+    if ($email) {
+        $user = $personnelRepository->findOneBy(['email' => $email]);
+        if ($user && $user->getBlockedUntil() !== null) {
+            $now = new \DateTime();
+            if ($user->getBlockedUntil() > $now) {
+                $remainingTime = $user->getBlockedUntil()->getTimestamp() - $now->getTimestamp();
+            }
+        }
     }
 
+    return $this->render('security/login.html.twig', [
+        'last_username' => $email,
+        'error' => null, // ou ton système d'erreur habituel
+        'remaining_time' => $remainingTime,
+    ]);
+}
+    
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    
 }
--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -2,11 +2,15 @@
 
 namespace App\Security;
 
+use App\Repository\PersonnelRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -21,33 +25,90 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
-    {
+    private PersonnelRepository $personnelRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        PersonnelRepository $personnelRepository,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->personnelRepository = $personnelRepository;
+        $this->entityManager = $entityManager;
     }
 
     public function authenticate(Request $request): Passport
     {
         $email = $request->getPayload()->getString('email');
-
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
 
+        $user = $this->personnelRepository->findOneBy(['email' => $email]);
+
+        if ($user) {
+            $now = new \DateTime();
+            if ($user->getBlockedUntil() !== null && $user->getBlockedUntil() > $now) {
+                $remaining = $user->getBlockedUntil()->getTimestamp() - $now->getTimestamp();
+                throw new CustomUserMessageAuthenticationException(
+                    'Compte bloqué temporairement. Réessayez dans ' . $remaining . ' secondes.'
+                );
+            }
+        }
+
         return new Passport(
-            new UserBadge($email),
+            new UserBadge($email, function ($userIdentifier) use ($request) {
+                $user = $this->personnelRepository->findOneBy(['email' => $userIdentifier]);
+
+                if (!$user) {
+                    throw new CustomUserMessageAuthenticationException('Email ou mot de passe incorrect.');
+                }
+
+                return $user;
+            }),
             new PasswordCredentials($request->getPayload()->getString('password')),
             [
-                new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),]
+                new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
+            ]
         );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
+        // Réinitialiser les compteurs en cas de succès
+        $user = $token->getUser();
+        if (method_exists($user, 'setLoginAttempts')) {
+            $user->setLoginAttempts(0);
+            $user->setBlockedUntil(null);
+            $this->entityManager->flush();
+        }
+
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
         return new RedirectResponse($this->urlGenerator->generate('app_accueil'));
-        throw new \Exception('TODO: provide a valid redirect inside ' . __FILE__);
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        $email = $request->getPayload()->getString('email');
+        $user = $this->personnelRepository->findOneBy(['email' => $email]);
+
+        if ($user) {
+            $now = new \DateTime();
+            $user->setLoginAttempts($user->getLoginAttempts() + 1);
+
+            // Blocage progressif : 3 tentatives = 30s, 4 = 1min, 5 = 2min, 6+ = 5min
+            if ($user->getLoginAttempts() >= 3) {
+                $durations = [30, 60, 120, 300]; // en secondes
+                $index = min($user->getLoginAttempts() - 3, count($durations) - 1);
+                $blockDuration = $durations[$index];
+                $user->setBlockedUntil((clone $now)->modify("+$blockDuration seconds"));
+            }
+
+            $this->entityManager->flush();
+        }
+
+        return parent::onAuthenticationFailure($request, $exception);
     }
 
     protected function getLoginUrl(Request $request): string
